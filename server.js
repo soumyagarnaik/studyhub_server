37cd37bbbb3288ca1f22bc37b169dd80572f3cbd
Soumya Ranjan Garnaik <72615798+soumyagarnaik@users.noreply.github.com>
2023-01-09 09:55:09 +0530

    Add cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import morgan from 'morgan'
+import cors from 'cors'
 import connectDB from './config/db.js'
 import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
@@ -22,6 +23,10 @@ if(process.env.NODE_ENV=== 'development'){
 }
 
 app.use(express.json())
+
+app.use(cors({
+  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
 
 app.get('/',(req,res)=>{
     res.send("Api running")
